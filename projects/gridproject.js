59fdcd5d3da62910f3406267e4bb38584f1d6c3c
robiat <robiatyetunde18@gmail.com>
2024-08-06 21:17:33 +0100

    work

---
+++ projects/gridproject.js
@@ -0,0 +1,8 @@
+const hamburger=document.querySelector(".hamburger");
+const bar1=document.querySelector(".bar1");
+const bar2=document.querySelector(".bar2");
+const bar3=document.querySelector(".bar3");
+
+hamburger.addEventListener("click", ()=>{
+    alert("CLICKED");
+})
